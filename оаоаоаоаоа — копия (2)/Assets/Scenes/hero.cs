9e97e96da0bb8cc3ecfc43fbfc579ebec25aad45
Mariaigutova <mariaigutova@gmail.com>
2024-11-13 05:50:50 +0300

    emeny1level
    .

--- оаоаоаоаоа — копия (2)/Assets/Scenes/hero.cs
+++ оаоаоаоаоа — копия (2)/Assets/Scenes/hero.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class Hero : MonoBehaviour
 {
     [SerializeField] private float speed = 3f; // скорость движения 
     [SerializeField] private int lives = 5; // количество жизней
-    [SerializeField] private float jumpForce = 15f; // сила прыжка
+    [SerializeField] private float jumpForce = 7f; // сила прыжка
     private bool isGrounded = false;
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private Camera mainCamera;
 
     private void FixedUpdate()
     {
@@ -23,9 +22,8 @@ public class Hero : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        mainCamera = Camera.main;
     }
-
-
 
     private void Update()
     {
@@ -35,17 +33,28 @@ public class Hero : MonoBehaviour
             Jump();
     }
 
-
     private void Run()
     {
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
         sprite.flipX = dir.x < 0.0f;
+
+
+        Vector3 screenPoint = mainCamera.WorldToScreenPoint(transform.position);
+        if (screenPoint.x < 0 || screenPoint.x > Screen.width)
+        {
+
+            transform.position = mainCamera.ScreenToWorldPoint(new Vector3(Mathf.Clamp(screenPoint.x, 0, Screen.width), screenPoint.y, screenPoint.z));
+        }
     }
 
     private void Jump()
     {
-        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        if (isGrounded)
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
     }
 
     private void CheckGround()
@@ -53,5 +62,5 @@ public class Hero : MonoBehaviour
         Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
         isGrounded = collider.Length > 1;
     }
-}
 
+}
---
+++ оаоаоаоаоа — копия (2)/Assets/enemysc.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
